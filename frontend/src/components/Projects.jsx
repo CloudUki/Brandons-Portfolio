1e1a465d3c94931d3f39db231827b68a8d3ac8a2
Brandon Yang <brandon.yang1210@gmail.com>
2025-06-27 14:24:13 -0400

    added the photography portfolio and changed descriptions on others, updated resume

--- frontend/src/components/Projects.jsx
+++ frontend/src/components/Projects.jsx
@@ -3,7 +3,7 @@ import { motion } from 'framer-motion';
 import mirrulations from '../assets/mirrulations.jpeg';
 import calcium from '../assets/caco_img.svg';
 import portfolio from '../assets/portfolio.jpeg';
-import photo from '../assets/SteelStacks.jpg';
+import photo from '../assets/photography.jpeg';
 import checkers from '../assets/checkers_img.svg';
 import moravian from '../assets/mu_ball_img.jpg';
 import date_one from '../assets/two_four.svg';
@@ -36,20 +36,20 @@ const projects = [
   {
     id: 'portfolio',
     title: 'Portfolio',
-    description: `Created and deployed a responsive personal site to showcase projects and hobbies, focused on clean UI/UX design.`,
+    description: `Designed and deployed a responsive website showcasing projects and hobbies focusing on intuitive design. Engineered a scalable, smooth user interface utilizing React, CSS, Vercel, NextJS, ViteJS, and GitHub. Leveraged Jest for unit testing to ensure component stability and maintain code quality.`,
     image: portfolio,
     date: date_two,
-    tech: ['React.js', 'ViteJS', 'Vercel', 'ExpressJS', 'NodeJS', 'CSS', 'GitHub'],
+    tech: ['React.js', 'ViteJS', 'Vercel', 'NextJS', 'CSS', 'GitHub'],
     deployed: 'https://brandons-portfolio.vercel.app/',
   },
   {
     id: "photography",
     title: "Photography Portfolio",
-    description: "Developed a web application highlighting my photography and facilitating professional communication.",
+    description: "Developed a web application highlighting my photography and facilitating professional communication. Implemented a fast, seamless user interface by developing with React, Vite, and NextJS. Leveraged Supabase storage for hosting and serving uploaded images in a photography portfolio.",
     image: photo,
     date: date_two,
-    tech: ['React', 'ViteJS', 'NextJS', 'Supabase', 'GitHub'],
-    github: 'https://github.com/CloudUki/Photography_Portfolio'
+    tech: ['React', 'ViteJS', 'NextJS', 'Supabase', 'Chakra-UI', 'GitHub'],
+    deployed: 'https://brandons-photography.vercel.app/'
   },
   {
     id: "checkers",
